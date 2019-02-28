af24aecf61a5593fc12139caf48ad122c04f7cb3
James Hines <jameshines10@gmail.com>
2019-02-28 08:02:44 -0600

    Initial commit

---
+++ src/main/java/com/jhinesconsulting/com/App.java
@@ -0,0 +1,13 @@
+package com.jhinesconsulting.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
