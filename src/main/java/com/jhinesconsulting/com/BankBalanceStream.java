8c429b16b0e44ad5bbf12a7531355d0e5aa55a40
James Hines <jameshines10@gmail.com>
2019-02-28 08:06:45 -0600

    Adds the class that will be used to build the stream topology

---
+++ src/main/java/com/jhinesconsulting/com/BankBalanceStream.java
@@ -0,0 +1,5 @@
+package com.jhinesconsulting.com;
+
+public class BankBalanceStream {
+    // { "name": "Casey", "balance": 5000, "numTransactions": 100, "latestTransaction": "2019-08-01 01:59:30.274" }
+}
