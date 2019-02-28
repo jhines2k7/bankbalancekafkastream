a98511f475e134033d1fc0c63caad216425b93c6
James Hines <jameshines10@gmail.com>
2019-02-28 08:07:29 -0600

    Adds main method

--- src/main/java/com/jhinesconsulting/com/BankBalanceStream.java
+++ src/main/java/com/jhinesconsulting/com/BankBalanceStream.java
@@ -2,4 +2,7 @@ package com.jhinesconsulting.com;
 
 public class BankBalanceStream {
     // { "name": "Casey", "balance": 5000, "numTransactions": 100, "latestTransaction": "2019-08-01 01:59:30.274" }
+    public static void main(String[] args) {
+
+    }
 }
